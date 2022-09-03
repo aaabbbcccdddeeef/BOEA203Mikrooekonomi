6082753e8d92b01946c58a110b090ecb96c3b2c3
joernih <jorn.halvorsen@gmail.com>
2022-09-03 14:32:05 +0200

    cropadj

--- inst/forelesningsnotater/drawio/figur.sh
+++ inst/forelesningsnotater/drawio/figur.sh
@@ -7,8 +7,8 @@ magick oversikt_prod_i.png -crop 275x210+1+55 prodkl.png
 magick oversikt_prod_i.png -crop 275x210+275+55 prodkk.png
 magick oversikt_prod_i.png -crop 275x210+275+55 isokvant.png
 magick oversikt_prod_i.png -crop 300x250+1+350 isokvant.png
-magick oversikt_prod_i.png -crop 300x250+1+350 linsub.png
-magick oversikt_prod_i.png -crop 300x250+1+350 lensub.png
-magick oversikt_prod_i.png -crop 300x250+1+350 okskala.png
-magick oversikt_prod_i.png -crop 300x250+1+350 avskala.png
-magick oversikt_prod_i.png -crop 300x250+1+350 koskala.png
+magick oversikt_prod_i.png -crop 300x225+1+600 linsub.png
+magick oversikt_prod_i.png -crop 320x310+1+800 lensub.png
+magick oversikt_prod_i.png -crop 1000x250+375+360 okskala.png
+magick oversikt_prod_i.png -crop 1000x250+375+360 koskala.png
+magick oversikt_prod_i.png -crop 1000x450+425+830 avskala.png
