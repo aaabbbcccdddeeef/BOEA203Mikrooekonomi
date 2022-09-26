98c3c283b6b21057573d4c1d3324e525afd572b3
joernih <jorn.halvorsen@gmail.com>
2022-09-26 13:26:51 +0200

    kap3first

--- inst/forelesningsnotater/drawio/figur_h.sh
+++ inst/forelesningsnotater/drawio/figur_h.sh
@@ -2,10 +2,10 @@
 
 echo 'converting'
 ## Nytteteori
-magick oversikt_kons_i.png -crop 1000x250+520+1 grensenytte.png
-magick oversikt_kons_i.png -crop 1000x250+520+1 indkurve.png
-magick oversikt_kons_i.png -crop 250x290+260+200 indkart.pngn
-magick oversikt_kons_i.png -crop 270x300+250+500 budsjbet.png
+magick oversikt_kons_i.png -crop 1000x235+520+20 grensenytte.png
+magick oversikt_kons_i.png -crop 1000x240+520+270 indkurve.png
+magick oversikt_kons_i.png -crop 250x250+1+530 indkart.png
+magick oversikt_kons_i.png -crop 270x255+240+537 budsjbet.png
 magick oversikt_kons_i.png -crop 270x300+550+500 opttilp.png
 magick oversikt_kons_i.png -crop 1000x250+520+1 subsperf.png
 magick oversikt_kons_i.png -crop 1000x250+520+1 subsnorm.png
