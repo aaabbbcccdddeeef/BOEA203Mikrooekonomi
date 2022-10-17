7bd6cb79085b48ea0ef9c53d11371bf87abe2715
joernih <jorn.halvorsen@gmail.com>
2022-10-17 14:29:30 +0200

    pubish

--- docs/articles/drawio/figur_m.sh
+++ docs/articles/drawio/figur_m.sh
@@ -3,11 +3,5 @@
 echo 'converting'
 ## Fullkommen konkurranse
 magick markedfullkommenanalyse.png -crop 1000x235+520+20 markl.png
-magick markedfullkommenanalyse.png -crop 1000x235+520+20 konso.png
-magick markedfullkommenanalyse.png -crop 1000x235+520+20 prodo.png
-magick markedfullkommenanalyse.png -crop 1000x235+520+20 planl.png
-magick markedfullkommenanalyse.png -crop 1000x235+520+20 avgfk.png
-
 ## Monopol
-magick markedmonopolanalyse.png -crop 1000x235+520+20 monoe.png
-magick markedmonopolanalyse.png -crop 1000x235+520+20 monop.png
+#magick oversikt_kons_i.png -crop 1000x235+520+20 markedfullkommenanalyse.png
